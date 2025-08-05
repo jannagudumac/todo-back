1f76fa4e83d3c1d18c579d0461b370c4ccd14179
triceratops777 <jgood.ulysses@gmail.com>
2025-08-05 12:32:03 +0200

    CorsConfig removed, change variable in SecurityConfig

--- src/main/java/com/descodeuses/planit/security/SecurityConfig.java
+++ src/main/java/com/descodeuses/planit/security/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     @Bean
     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of(allowCorsOrigin));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
